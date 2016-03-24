98b1e0bd0a35fe7bd0f29f07255f122997c573e3
zjuturtle <leijinghao@yeah.net>
2016-03-24 10:03:15 +0800

    338

---
+++ Ex338/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> countBits(int num) {
+	vector<int> res;
+	res.push_back(0);
+	if (num == 0) {
+		return res;
+	}
+	res.push_back(1);
+	if (num == 1) {
+		return res;
+	}
+	while (true) {
+		int size = res.size();
+		for (int i = 0; i < size; i++) {
+			res.push_back(res[i] + 1);
+			if (res.size() == num+1) {
+				return res;
+			}
+		}
+	}
+	
+}
+int main() {
+	vector<int> res = countBits(5);
+	for (auto it = res.begin(); it != res.end(); ++it)
+		cout << (*it) << endl;
+	getchar();
+	return 0;
+}
